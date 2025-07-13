023e0d502e44e9a6df3354ad43bcd256789d7bb3
Lawrence Mulligan <lmulligan@lpmnet.com>
2025-07-13 11:49:54 -0500

    Removed sleep

--- pre_bootstrap.sh
+++ pre_bootstrap.sh
@@ -13,7 +13,6 @@ chmod a+x ~/.local/bin/yadm
 ~/.local/bin/yadm clone --bootstrap -f https://github.com/lpmulligan/.dotfiles.git
 
 # Clean up by removing yadm
-sleep 7
 rm -rf ~/.local/bin/yadm
 
 echo "Dotfiles setup completed!"
